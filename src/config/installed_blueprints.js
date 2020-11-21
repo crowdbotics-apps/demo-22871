3c2f43fb2de16dbd8eeb447a646dccce16cb4239
Crowdbotics <team@crowdbotics.com>
2020-11-21 21:14:34 +0000

    add package blueprint-react-native-sign-up-2-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'SignUp23177183', human_name: 'Sign Up 2 3', access_route: 'SignUp23177183'},
 { name: 'BlankScreen0177182', human_name: 'Blank Screen 0', access_route: 'BlankScreen0177182'},
 
   // you can add more installed blueprints here
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import SignUp23177183Navigator from '../features/SignUp23177183/navigator';
 import BlankScreen0177182Navigator from '../features/BlankScreen0177182/navigator';
 
 /**
@@ -14,6 +15,7 @@ import BlankScreen0177182Navigator from '../features/BlankScreen0177182/navigato
 const AppNavigator = {
 
     //@BlueprintNavigationInsertion
+SignUp23177183: { screen: SignUp23177183Navigator },
 BlankScreen0177182: { screen: BlankScreen0177182Navigator },
 
     /** new navigators can be added here */
