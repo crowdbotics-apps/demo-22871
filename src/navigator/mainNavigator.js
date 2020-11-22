610619da585c79359760bb76c812bbdc289d733f
Crowdbotics <team@crowdbotics.com>
2020-11-22 02:47:15 +0000

    remove package blueprint-react-native-notifications-screen

--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -10,7 +10,6 @@ import ArticleList177191Navigator from '../features/ArticleList177191/navigator'
 import ArticleList177190Navigator from '../features/ArticleList177190/navigator';
 import UserProfile4177186Navigator from '../features/UserProfile4177186/navigator';
 import Feed1177185Navigator from '../features/Feed1177185/navigator';
-import NotificationList2177184Navigator from '../features/NotificationList2177184/navigator';
 import SignUp23177183Navigator from '../features/SignUp23177183/navigator';
 
 /**
@@ -25,7 +24,6 @@ ArticleList177191: { screen: ArticleList177191Navigator },
 ArticleList177190: { screen: ArticleList177190Navigator },
 UserProfile4177186: { screen: UserProfile4177186Navigator },
 Feed1177185: { screen: Feed1177185Navigator },
-NotificationList2177184: { screen: NotificationList2177184Navigator },
 SignUp23177183: { screen: SignUp23177183Navigator },
 
     /** new navigators can be added here */
